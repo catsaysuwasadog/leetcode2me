c88bc82b5f28b743012242504fa17901b022f23e
iduosi <catsaysuwasadog@gmail.com>
2019-04-02 22:53:03 +0800

    add pycharm's ide-files path to git ignore.

---
+++ pythonlearn/thread/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Copyright (c) CH, All rights reserved. Licensed by iduosi@icloud.com
+
+# from __future__ import absolute_import, division, print_function
+# from __future__ import unicode_literals
+
+import queue
+import threading
+
